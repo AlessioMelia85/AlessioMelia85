2215a96963beb7b175a87d617faf6ce29f434e7e
Alessio <alessio.melia85@gmail.com>
2020-03-26 11:47:15 +0100

    new commit

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 1, name: 'Monkey D. Luffy', rule: 'Captain' },
+  { id: 2, name: 'Roronoa Zoro', rule: 'Swordman'},
+  { id: 3, name: 'Nami' , rule: 'Navigator'},
+  { id: 4, name: 'Usopp', rule: 'Sniper'},
+  { id: 5, name: 'Winsmoke Sanji', rule: 'Chef'},
+  { id: 6, name: 'Tony Tony Chopper' , rule: 'Doctor'},
+  { id: 7, name: 'Nico Robin' , rule: 'Archeologist'},
+  { id: 8, name: 'Cutty Flame(Franky)' , rule: 'Carpenter'},
+  { id: 9, name: 'Brook' , rule: 'Musician'},
+  { id: 10, name: 'Jinbey' , rule: 'Fishman'}
 ];
